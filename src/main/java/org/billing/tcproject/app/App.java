b22870d609b197900488804f79e4a84b5b0b4aa2
Mikhail.Chernichenko <mchernichenko@mail.ru>
2015-12-17 23:48:37 +0300

    fix from idea

--- src/main/java/org/billing/tcproject/app/App.java
+++ src/main/java/org/billing/tcproject/app/App.java
@@ -1,4 +1,4 @@
-package ru.billing.app;
+package org.billing.tcproject.app;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! 1" );
     }
 }
